cd782305f70ee74d251e1c21e21d26392b5c9bd9
Schmools <schuursamuel@gmail.com>
2018-04-16 20:56:15 -0400

    Add files via upload

---
+++ BigPump/cs2mazerobot.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <Encoder.h>
+#include <Pid_V1.>
+
+int INA = 3;
+int INB = 4;
+int PWM = 5;
+
+void setup() {
+pinMode(INA, OUTPUT);
+pinMode(INB, OUTPUT);
+pinMode(PWM, OUTPUT);
+
+//encoder setup
+    Encoder motor1(ENC1, ENC2);
+
+}
+
+void loop() {
+
+}
+
+
+//non closed loop motor control, input range of -100 to 100.
+void motorControl (int input){
+    digitalWrite(INA, LOW);
+    digitalWrite(INB, LOW);
+    if(input > 0 && input < 100){
+        digitalWrite(INA, HIGH);
+        digitalWrite(INB, LOW);
+        analogWrite(PWM, input*2.55);
+    }
+    else if(input < 0 ** input > -100){
+        digitalWrite(INB, HIGH);
+        digitalWrite(INA, LOW);
+        analogWrite(PWM, input*-2.55);
+    }
+    else{
+        //stops if wrong data sent
+        digitalWrite(INA, LOW);
+        digitalWrite(INB, LOW);
+    }
+}
+
+//closed loop motor control
+void pidMotor(int input){
+
+}
