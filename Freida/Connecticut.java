f00989d505cef12da0286100750eba09810e259b
juliaro16 <38625833+juliaro16@users.noreply.github.com>
2018-05-13 21:05:05 -0400

    Add files via upload

---
+++ Freida/Connecticut.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SaintLouis here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Connecticut extends Actor
+{
+    /**
+     * Act - do whatever the SaintLouis wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    GreenfootSound connecticut = new GreenfootSound("Connecticut.wav");
+    MouseInfo mouse = Greenfoot.getMouseInfo();  
+    boolean mouseOver = false;
+    public void act() 
+    {    
+       if (!mouseOver && Greenfoot.mouseMoved(this))  
+       {  
+            setImage("buttonHover.png");  
+            mouseOver = true;  
+       }  
+       if (mouseOver && Greenfoot.mouseMoved(null) && ! Greenfoot.mouseMoved(this))  
+       {  
+            setImage("button.png");  
+            mouseOver = false;  
+       } 
+       if(Greenfoot.mouseClicked(this)){
+            setImage("buttonPressed.png");
+            connecticut.play();
+       }
+        // Add your action code here.
+    }    
+}
